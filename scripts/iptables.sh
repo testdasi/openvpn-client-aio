86103d06f5851320ca212ad6a1c85ec2a357f73c
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 00:59:28 +0100

    Detect local net and set ip table for local + host

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -6,8 +6,8 @@ ETH0_IP="$(ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
 ETH0_RANGE=${ETH0_IP:(-3)}
 #Length of IP (to get network from sipcalc
 ETH0_IPLEN=${#ETH0_IP} ; let ETH0_IPLEN-=3
-#Use sipcalc to extract first IP (.0) of network
-ETH0_NET0="$(sipcalc $ETH0_IP | grep 'Network address')" ; ETH0_NET0=${ETH0_NET:(-$ETH0_IPLEN)} ; ETH0_NET0="$(echo $ETH0_NET | sed 's/ //g')" ; ETH0_NET0="$(echo $ETH0_NET | sed 's/-//g')"
+#Use sipcalc to get first IP (.0) of network and sed to clean up resulting string
+ETH0_NET0="$(sipcalc $ETH0_IP | grep 'Network address')" ; ETH0_NET0=${ETH0_NET0:(-$ETH0_IPLEN)} ; ETH0_NET0="$(echo $ETH0_NET0 | sed 's/ //g')" ; ETH0_NET0="$(echo $ETH0_NET0 | sed 's/-//g')"
 #Network in CIDR format
 ETH0_NET="$ETH0_NET0$ETH0_RANGE"
 echo "[info] eth0 IP is $ETH0_IP in network $ETH0_NET"
@@ -24,11 +24,13 @@ echo '[info] Unblock tunnel'
 iptables -A INPUT -i tun0 -j ACCEPT
 iptables -A OUTPUT -o tun0 -j ACCEPT
 
-echo '[info] Unblock internal comm'
-#-A INPUT -s 172.71.1.0/24 -d 172.71.1.0/24 -j ACCEPT
-#-A INPUT -s 10.132.6.0/23 -d 10.132.6.0/23 -j ACCEPT
-#-A INPUT -s 10.132.6.0/23 -d 172.71.1.0/24 -i eth0 -p tcp -j ACCEPT
-#-A INPUT -s 10.132.6.0/23 -d 10.132.6.0/23 -i eth0 -p tcp -j ACCEPT
+echo "[info] Unblock $ETH0_NET"
+iptables -A INPUT -s $ETH0_NET -d $ETH0_NET -j ACCEPT
+iptables -A OUTPUT -s $ETH0_NET -d $ETH0_NET -j ACCEPT
+
+echo "[info] Unblock traffic between $ETH0_NET and host"
+iptables -A INPUT -s ${HOST_NETWORK} -d $ETH0_NET -i eth0 -p tcp -j ACCEPT
+iptables -A OUTPUT -s $ETH0_NET -d ${HOST_NETWORK} -o eth0 -p tcp -j ACCEPT
 
 echo '[info] Unblock icpm outgoing (pings)'
 iptables -A INPUT  -p icmp -m state --state ESTABLISHED,RELATED     -j ACCEPT
