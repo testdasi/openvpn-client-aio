6bc166cf749f6fd05277f5dda8f8634704c31599
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 00:27:26 +0100

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-SERVER_IP="$(ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
+ETH0_IP="$(ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
+ETH0_RANGE=${ETH0_NET:(-3)}
+ETH0_NET="$(sipcalc $ETH0_IP | grep 'Network address')"
+ETH0_NET=${ETH0_NET:(-15)}
+ETH0_NET="$(echo $ETH0_NET | sed 's/ //g')"
+ETH0_NET="$(echo $ETH0_NET | sed 's/-//g')"
+echo "[info] $ETH0_IP is in $ETH0_NET$ETH0_RANGE"
 
 echo '[info] Block everything (unless unblock specifically)'
 iptables -P INPUT DROP
@@ -13,6 +19,12 @@ iptables -A OUTPUT -o lo -j ACCEPT
 echo '[info] Unblock tunnel'
 iptables -A INPUT -i tun0 -j ACCEPT
 iptables -A OUTPUT -o tun0 -j ACCEPT
+
+echo '[info] Unblock internal comm'
+-A INPUT -s 172.71.1.0/24 -d 172.71.1.0/24 -j ACCEPT
+-A INPUT -s 10.132.6.0/23 -d 10.132.6.0/23 -j ACCEPT
+-A INPUT -s 10.132.6.0/23 -d 172.71.1.0/24 -i eth0 -p tcp -j ACCEPT
+-A INPUT -s 10.132.6.0/23 -d 10.132.6.0/23 -i eth0 -p tcp -j ACCEPT
 
 echo '[info] Unblock icpm outgoing (pings)'
 iptables -A INPUT  -p icmp -m state --state ESTABLISHED,RELATED     -j ACCEPT
