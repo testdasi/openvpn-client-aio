39f24c06ef230455525e4f7e9146f5a19362e87c
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 20:39:29 +0100

    Improve VPN IP detection, add port to Info echo

--- scripts/openvpn.sh
+++ scripts/openvpn.sh
@@ -7,14 +7,14 @@ if [ ! -c /dev/net/tun ]; then
 fi
 
 openvpn --daemon --cd /etc/openvpn --config openvpn.ovpn
-echo '[info] Connecting to VPN...'
+echo '[info] Connecting to VPN on port $OPENVPN_PORT...'
 
-iphiden=$(dig +short myip.opendns.com @208.67.222.222)
-while [ -z "$iphiden" ]
+iphiden=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
+while [[ $iphiden =~ "timed out" ]]
 do 
     echo '[info] Connection in progress, wait 10s...'
     sleep 10
-    iphiden=$(dig +short myip.opendns.com @208.67.222.222)
+    iphiden=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
 done
 echo "[info] Your VPN public IP is $iphiden"
 
