93984fbb727d832b9d215086000d093a20f03c74
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 23:30:12 +0100

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-#echo '[info] Add route to local LAN'
-#echo "add route $HOST_LAN via $HOST_IP"
-#ip route add $HOST_LAN via $HOST_IP
+SERVER_IP="$(ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
 
 echo '[info] Block everything (unless unblock specifically)'
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 
-echo '[info] Unblock tunnel both ways'
+echo '[info] Unblock loopback'
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
+
+echo '[info] Unblock tunnel'
 iptables -A INPUT -i tun0 -j ACCEPT
 iptables -A OUTPUT -o tun0 -j ACCEPT
 
-echo '[info] Unblock OpenVPN Port'
-iptables -A INPUT -p udp --dport $OPENVPN_PORT -j ACCEPT
-iptables -A INPUT -p udp --sport $OPENVPN_PORT -j ACCEPT
-iptables -A OUTPUT -p udp --sport $OPENVPN_PORT -j ACCEPT
-iptables -A OUTPUT -p udp --dport $OPENVPN_PORT -j ACCEPT
+echo '[info] Unblock icpm outgoing (pings)'
+iptables -A INPUT  -p icmp -m state --state ESTABLISHED,RELATED     -j ACCEPT
+iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+echo '[info] Unblock OpenVPN outgoing'
+iptables -A INPUT  -p udp --sport $OPENVPN_PORT -m state --state ESTABLISHED     -j ACCEPT
+iptables -A OUTPUT -p udp --dport $OPENVPN_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 
 echo '[info] Unblock DNS inbound from eth0'
-iptables -A INPUT -i eth0 -p tcp --dport $DNS_PORT -j ACCEPT
-iptables -A INPUT -i eth0 -p udp --dport $DNS_PORT -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport $DNS_PORT -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp --sport $DNS_PORT -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp --dport $DNS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport $DNS_PORT -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p udp --dport $DNS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p udp --sport $DNS_PORT -m state --state ESTABLISHED -j ACCEPT
 
 echo '[info] Unblock dante in bound from eth0'
 iptables -A INPUT -i eth0 -p tcp --dport $DANTE_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
@@ -32,13 +34,13 @@ iptables -A OUTPUT -o eth0 -p tcp --sport $DANTE_PORT -m state --state ESTABLISH
 iptables -A OUTPUT -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 echo '[info] Unblock tinyproxy inbound from eth0'
-iptables -A INPUT -i eth0 -p tcp --dport $TINYPROXY_PORT -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport $TINYPROXY_PORT -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp --dport $TINYPROXY_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport $TINYPROXY_PORT -m state --state ESTABLISHED -j ACCEPT
 
 echo '[info] Unblock torsocks inbound from eth0'
-iptables -A INPUT -i eth0 -p tcp --dport $TORSOCKS_PORT -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport $TORSOCKS_PORT -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp --dport $TORSOCKS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport $TORSOCKS_PORT -m state --state ESTABLISHED -j ACCEPT
 
 echo '[info] Unblock privoxy inbound from eth0'
-iptables -A INPUT -i eth0 -p tcp --dport $PRIVOXY_PORT -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport $PRIVOXY_PORT -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp --dport $PRIVOXY_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport $PRIVOXY_PORT -m state --state ESTABLISHED -j ACCEPT
