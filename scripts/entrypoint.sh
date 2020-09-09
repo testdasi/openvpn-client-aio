ee08171002b756c186c8808e6148a0d925e30756
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 22:04:37 +0100

    Move openvpn section up and fix_config.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,53 +1,36 @@
 #!/bin/bash
 
-### Fixed DNS Port ###
-DNS_PORT=53
-
-### Dynamically determine OPENVPN_PORT based on config file ###
-#OPENVPN_PORT=1198
-echo ''
-echo '[info] Determine openvpn port from config file'
-OPENVPN_PORT=$(grep -m 1 "remote " /etc/openvpn/openvpn.ovpn)
-OPENVPN_PORT=${OPENVPN_PORT:(-5)}
-
-### Adjusting config with actual port ###
+### Fixing config files ###
 echo ''
 echo '[info] Fixing configs'
-DANTE_PORT=${SOCKS_PROXY_PORT}
-sed -i "s|internal: eth0 port=1080|internal: eth0 port=$DANTE_PORT|g" '/etc/danted.conf'
-echo '[info] danted fixed'
-TINYPROXY_PORT=${HTTP_PROXY_PORT}
-sed -i "s|Port 8080|Port $TINYPROXY_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
-echo '[info] tinyproxy fixed'
-TORSOCKS_PORT=${TOR_SOCKS_PORT}
-sed -i "s|SOCKSPort 0\.0\.0\.0:9050|SOCKSPort 0\.0\.0\.0:$TORSOCKS_PORT|g" '/etc/tor/torrc'
-echo '[info] torsocks fixed'
-PRIVOXY_PORT=${TOR_HTTP_PORT}
-sed -i "s|listen-address 0\.0\.0\.0:8118|listen-address 0\.0\.0\.0:$PRIVOXY_PORT|g" '/etc/privoxy/config'
-sed -i "s|forward-socks5t \/ localhost:9050|forward-socks5t \/ localhost:$TORSOCKS_PORT|g" '/etc/privoxy/config'
-echo '[info] privoxy fixed'
+source /fix_config.sh
 echo '[info] All configs fixed'
 
 ### Stubby DNS-over-TLS ###
 echo ''
+DNS_PORT=53
 echo "[info] Run stubby in background on port $DNS_PORT"
 stubby -g -C /etc/stubby/stubby.yml
 ipnaked=$(dig +short myip.opendns.com @208.67.222.222)
 echo "[warn] Your ISP public IP is $ipnaked"
 
-### IP Tables ###
+### OpenVPN ###
+echo ''
+echo "[info] Setting up OpenVPN tunnel"
+echo '[info] Determine openvpn port from config file'
+OPENVPN_PORT=$(grep -m 1 "remote " /etc/openvpn/openvpn.ovpn)
+OPENVPN_PORT=${OPENVPN_PORT:(-5)}
+source /openvpn.sh
+echo '[info] Done'
+
+### IPtable ###
 echo ''
 echo '[info] Set up IP tables'
 source /iptables.sh
 echo '[info] All rules created'
-ipttest=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
-echo "[info] Quick block test. Expected result is time out. Actual result is $ipttest"
-
-### OpenVPN ###
-echo ''
-echo "[info] Setting up OpenVPN tunnel"
-source /openvpn.sh
-echo '[info] Connected!'
+## IPtable block test for testing purposes only ##
+#ipttest=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
+#echo "[info] Quick block test. Expected result is time out. Actual result is $ipttest"
 
 ### Dante SOCKS proxy to VPN ###
 echo ''
