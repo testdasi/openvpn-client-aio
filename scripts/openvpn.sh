2e1b3a7d5f46fe991cac9eba58b21d3c76729914
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 19:48:14 +0100

    Update openvpn.sh

--- scripts/openvpn.sh
+++ scripts/openvpn.sh
@@ -8,8 +8,8 @@ fi
 
 echo "[info] Allow DnS-over-TLS for openvpn to lookup VPN server"
 echo 'nameserver 127.2.2.2' > /etc/resolv.conf
-iptables -A INPUT  -p tcp --sport $DOT_PORT -j ACCEPT
-iptables -A OUTPUT -p tcp --dport $DOT_PORT -j ACCEPT
+nft add rule ip filter INPUT  tcp sport $DOT_PORT counter accept
+nft add rule ip filter OUTPUT tcp dport $DOT_PORT counter accept
 
 echo "[info] Connecting to VPN on port $OPENVPN_PORT with proto $OPENVPN_PROTO..."
 openvpn --daemon --cd /etc/openvpn --config openvpn.ovpn
@@ -23,8 +23,10 @@ done
 echo "[info] Your VPN public IP is $iphiden"
 
 echo "[info] Block DnS-over-TLS to force traffic through tunnel"
-iptables -D OUTPUT -p tcp --dport $DOT_PORT -j ACCEPT
-iptables -D INPUT  -p tcp --sport $DOT_PORT -j ACCEPT
+rulehandle="$(nft list table filter -a | grep "tcp sport $DOT_PORT")" ; rulehandle=${rulehandle:(-2)}
+nft delete rule filter INPUT handle $rulehandle
+rulehandle="$(nft list table filter -a | grep "tcp dport $DOT_PORT")" ; rulehandle=${rulehandle:(-2)}
+nft delete rule filter OUTPUT handle $rulehandle
 
 echo "[info] Change DNS servers to ${DNS_SERVERS}"
 # split comma seperated string into list from DNS_SERVERS env variable
@@ -41,8 +43,8 @@ for dns_server_item in "${dns_server_list[@]}"; do
     
     #resolv.conf only supports port 53
     echo "[info] Allowing DNS lookups (tcp, udp port 53) to server '${dns_server_item}'"
-    iptables -A OUTPUT -p udp -d ${dns_server_item} --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-    iptables -A INPUT  -p udp -s ${dns_server_item} --sport 53 -m state --state ESTABLISHED     -j ACCEPT
-    iptables -A OUTPUT -p tcp -d ${dns_server_item} --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-    iptables -A INPUT  -p tcp -s ${dns_server_item} --sport 53 -m state --state ESTABLISHED     -j ACCEPT
+    nft add rule ip filter INPUT  ip saddr ${dns_server_item} tcp sport 53 ct state established     counter accept
+    nft add rule ip filter OUTPUT ip daddr ${dns_server_item} tcp dport 53 ct state new,established counter accept
+    nft add rule ip filter INPUT  ip saddr ${dns_server_item} udp sport 53 ct state established     counter accept
+    nft add rule ip filter OUTPUT ip daddr ${dns_server_item} udp dport 53 ct state new,established counter accept
 done
