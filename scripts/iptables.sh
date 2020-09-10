af54c15bcedf64174f7e5946e94c8cc57340bb4a
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 18:34:57 +0100

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo '[info] Add missing route (if error, it means route already exists)'
+add_route="$(ip route | grep 'default')" ; add_route="$(sed "s|default|$HOST_NETWORK|g" <<< $add_route)"
+ip route add $add_route
+echo "[info] Added: $add_route"
+
 echo '[info] Block everything (unless unblock explicitly)'
 iptables -P INPUT DROP
 #iptables -P FORWARD DROP
