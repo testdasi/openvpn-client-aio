d96350d040d5280db056ad6e2fd0a81125aef89d
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 10:48:54 +0100

    Update set_variables.sh

--- scripts/set_variables.sh
+++ scripts/set_variables.sh
@@ -10,7 +10,7 @@ echo '[info] Determine openvpn port from config file'
 OPENVPN_PORT=$(grep -m 1 "remote " /etc/openvpn/openvpn.ovpn) ; OPENVPN_PORT=${OPENVPN_PORT:(-5)} ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ //g')"
 echo '[info] Determine openvpn protocol from config file'
 OPENVPN_PROTO=$(grep -m 1 "proto " /etc/openvpn/openvpn.ovpn) ; OPENVPN_PROTO="$(echo $OPENVPN_PROTO | sed 's/proto //g')"
-echo "[info] port=$OPENVPN_PORT proto=$OPENVPN_PROTO"
+echo "[info] Will connect openvpn on port=$OPENVPN_PORT proto=$OPENVPN_PROTO"
 
 ### Dynamically determine eth0 network for iptables ###
 echo '[info] Determine eth0 network for iptables'
