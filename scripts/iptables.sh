2f084918a8100ba1ff86dc728d04a628ec5021b8
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 01:37:56 +0100

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -12,8 +12,9 @@ ETH0_NET0="$(sipcalc $ETH0_IP | grep 'Network address')" ; ETH0_NET0=${ETH0_NET0
 ETH0_NET="$ETH0_NET0$ETH0_RANGE"
 echo "[info] eth0 IP is $ETH0_IP in network $ETH0_NET"
 
-echo '[info] Block everything (unless unblock specifically)'
+echo '[info] Block everything (unless unblock explicitly)'
 iptables -P INPUT DROP
+#iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 
 echo '[info] Unblock loopback'
@@ -36,29 +37,29 @@ echo '[info] Unblock icpm outgoing (pings)'
 iptables -A INPUT  -p icmp -m state --state ESTABLISHED,RELATED     -j ACCEPT
 iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
-echo '[info] Unblock OpenVPN outgoing'
+echo "[info] Unblock OpenVPN outgoing on $OPENVPN_PROTO$OPENVPN_PORT"
 iptables -A INPUT  -p $OPENVPN_PROTO --sport $OPENVPN_PORT -m state --state ESTABLISHED     -j ACCEPT
 iptables -A OUTPUT -p $OPENVPN_PROTO --dport $OPENVPN_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 
-echo '[info] Unblock DNS inbound from eth0'
+echo "[info] Unblock DNS inbound from eth0 on $DNS_PORT"
 iptables -A INPUT -i eth0 -p tcp --dport $DNS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport $DNS_PORT -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --dport $DNS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p udp --sport $DNS_PORT -m state --state ESTABLISHED -j ACCEPT
 
-echo '[info] Unblock dante in bound from eth0'
+echo "[info] Unblock dante in bound from eth0 on $DANTE_PORT"
 iptables -A INPUT -i eth0 -p tcp --dport $DANTE_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport $DANTE_PORT -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-echo '[info] Unblock tinyproxy inbound from eth0'
+echo "[info] Unblock tinyproxy inbound from eth0 on $TINYPROXY_PORT"
 iptables -A INPUT -i eth0 -p tcp --dport $TINYPROXY_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport $TINYPROXY_PORT -m state --state ESTABLISHED -j ACCEPT
 
-echo '[info] Unblock torsocks inbound from eth0'
+echo "[info] Unblock torsocks inbound from eth0 on $TORSOCKS_PORT"
 iptables -A INPUT -i eth0 -p tcp --dport $TORSOCKS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport $TORSOCKS_PORT -m state --state ESTABLISHED -j ACCEPT
 
-echo '[info] Unblock privoxy inbound from eth0'
+echo "[info] Unblock privoxy inbound from eth0 on $PRIVOXY_PORT"
 iptables -A INPUT -i eth0 -p tcp --dport $PRIVOXY_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport $PRIVOXY_PORT -m state --state ESTABLISHED -j ACCEPT
