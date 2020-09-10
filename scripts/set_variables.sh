9305d1eca3e9c05fbd676683d570342643dbe768
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 11:00:22 +0100

    Move port settings over

--- scripts/set_variables.sh
+++ scripts/set_variables.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 ### DNS and DoT ports are fixed ###
-echo '[info] Set DNS ports'
-DNS_PORT=53
+echo '[info] Set various ports to docker variables'
+DNS_PORT=${DNS_SERVER_PORT}
+DANTE_PORT=${SOCKS_PROXY_PORT}
+TINYPROXY_PORT=${HTTP_PROXY_PORT}
+TORSOCKS_PORT=${TOR_SOCKS_PORT}
+PRIVOXY_PORT=${TOR_HTTP_PORT}
+# DoT port is fixed due to TLS protocol
 DOT_PORT=853
 
 ### Dynamically determine OpenVPN port and protocol ###
