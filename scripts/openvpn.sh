81127230856670c6c1350ec03b30b003e783adc8
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 01:40:53 +0100

    Update openvpn.sh

--- scripts/openvpn.sh
+++ scripts/openvpn.sh
@@ -6,11 +6,11 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
-echo '[info] Temp unblock of DnS-over-TLS port to allow DNS lookup'
-iptables -A OUTPUT -p tcp --dport 853 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT  -p tcp --sport 853 -m state --state ESTABLISHED     -j ACCEPT
+echo '[info] Unblock DnS-over-TLS port to allow DNS lookup'
+iptables -A OUTPUT -p tcp --dport $DOT_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT  -p tcp --sport $DOT_PORT -m state --state ESTABLISHED     -j ACCEPT
 
-echo "[info] Connecting to VPN on port $OPENVPN_PORT..."
+echo "[info] Connecting to VPN on port $OPENVPN_PROTO$OPENVPN_PORT..."
 openvpn --daemon --cd /etc/openvpn --config openvpn.ovpn
 iphiden=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
 while [[ $iphiden =~ "timed out" ]]
@@ -22,8 +22,8 @@ done
 echo "[info] Your VPN public IP is $iphiden"
 
 echo '[info] Block DnS-over-TLS port'
-iptables -D OUTPUT -p tcp --dport 853 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -D INPUT  -p tcp --sport 853 -m state --state ESTABLISHED     -j ACCEPT
+iptables -D OUTPUT -p tcp --dport $DOT_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -D INPUT  -p tcp --sport $DOT_PORT -m state --state ESTABLISHED     -j ACCEPT
 
 echo "[info] Change DNS servers to ${DNS_SERVERS}"
 # split comma seperated string into list from DNS_SERVERS env variable
@@ -39,8 +39,8 @@ for dns_server_item in "${dns_server_list[@]}"; do
     echo "nameserver ${dns_server_item}" >> /etc/resolv.conf
     
     echo "[info] Allowing DNS lookups (tcp, udp port 53) to server '${dns_server_item}'"
-    iptables -A OUTPUT -p udp -d ${dns_server_item} --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-    iptables -A INPUT  -p udp -s ${dns_server_item} --sport 53 -m state --state ESTABLISHED     -j ACCEPT
-    iptables -A OUTPUT -p tcp -d ${dns_server_item} --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-    iptables -A INPUT  -p tcp -s ${dns_server_item} --sport 53 -m state --state ESTABLISHED     -j ACCEPT
+    iptables -A OUTPUT -p udp -d ${dns_server_item} --dport $DNS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A INPUT  -p udp -s ${dns_server_item} --sport $DNS_PORT -m state --state ESTABLISHED     -j ACCEPT
+    iptables -A OUTPUT -p tcp -d ${dns_server_item} --dport $DNS_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A INPUT  -p tcp -s ${dns_server_item} --sport $DNS_PORT -m state --state ESTABLISHED     -j ACCEPT
 done
