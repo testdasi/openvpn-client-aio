d6e92cbe4cd4a2595a65a7d6f642c41f2b0e7cfe
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-11 13:56:50 +0100

    Remove non-numeric characters from OPENVPN_PORT

--- scripts/set_variables.sh
+++ scripts/set_variables.sh
@@ -12,7 +12,7 @@ DOT_PORT=853
 
 ### Dynamically determine OpenVPN port and protocol ###
 echo '[info] Determine openvpn port from config file'
-OPENVPN_PORT=$(grep -m 1 "remote " /etc/openvpn/openvpn.ovpn) ; OPENVPN_PORT=${OPENVPN_PORT:(-5)} ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ //g')"
+OPENVPN_PORT=$(grep -m 1 "remote " /etc/openvpn/openvpn.ovpn) ; OPENVPN_PORT=${OPENVPN_PORT:(-5)} ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ //g')" ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/[^0-9]*//g')"
 echo '[info] Determine openvpn protocol from config file'
 OPENVPN_PROTO=$(grep -m 1 "proto " /etc/openvpn/openvpn.ovpn) ; OPENVPN_PROTO="$(echo $OPENVPN_PROTO | sed 's/proto //g')"
 echo "[info] Will connect openvpn on port=$OPENVPN_PORT proto=$OPENVPN_PROTO"
