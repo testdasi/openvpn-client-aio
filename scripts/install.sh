ea33962d862488b8b849348df201cb476fca68bb
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-15 12:44:36 +0100

    torless install option

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-# add various configs
+# add various base configs
 cd /tmp \
     && mkdir -p /etc/stubby \
     && cp -n ./stubby.yml /etc/stubby/ \
     && mkdir -p /etc/tinyproxy \
     && cp -n ./tinyproxy.conf /etc/tinyproxy/ \
-    && mkdir -p /etc/tor \
-    && cp -n ./torrc /etc/tor/ \
-    && mkdir -p /etc/privoxy \
-    && cp -n ./privoxy /etc/privoxy/config \
     && cp -n ./danted.conf /etc/
+
+# add tor and privoxy depending on torless tag
+if [[ ${TAG} =~ "torless" ]]
+then
+    cd /tmp \
+    && rm -fr ./torrc \
+    && rm -fr ./privoxy
+    echo "[info] Don't install torsocks and privoxy due to tag ${TAG}"
+else
+    # install torsocks and privoxy
+    apt-get -y update \
+    && apt-get -y install torsocks privoxy \
+    && mkdir -p /etc/tor \
+    && rm -rf /etc/tor/* \
+    && mkdir -p /etc/privoxy \
+    && rm -rf /etc/privoxy/*
+    
+    # install config files
+    cd /tmp \
+    && cp -n ./torrc /etc/tor/ \
+    && cp -n ./privoxy /etc/privoxy/config
+    
+    echo "[info] Installed torsocks and privoxy due to tag ${TAG}"
+fi
 
 # clean up
 apt-get -y autoremove \
