45255226b50150a6cf76a9c945333e357505aaa1
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-15 13:46:12 +0100

    Update fix_config.sh

--- scripts/fix_config.sh
+++ scripts/fix_config.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 sed -i "s|  - 0\.0\.0\.0\@53|  - 0\.0\.0\.0\@$DNS_PORT|g" '/etc/stubby/stubby.yml'
-echo '[info] stubby fixed'
+echo '[info] stubby fixed.'
 
 sed -i "s|internal: eth0 port=1080|internal: eth0 port=$DANTE_PORT|g" '/etc/danted.conf'
-echo '[info] danted fixed'
+echo '[info] danted fixed.'
 
 sed -i "s|Port 8080|Port $TINYPROXY_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
 sed -i "s|upstream socks5 localhost:1080|upstream socks5 $ETH0_IP:$DANTE_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
-echo '[info] tinyproxy fixed'
+echo '[info] tinyproxy fixed.'
 
 ### Fix TOR+Privoxy depending on build ###
 if [[ -f "/usr/sbin/tor" ]]; then
-    echo "[info] Tor build detected."
+    echo '[info] Tor build detected.'
     sed -i "s|SOCKSPort 0\.0\.0\.0:9050|SOCKSPort 0\.0\.0\.0:$TORSOCKS_PORT|g" '/etc/tor/torrc'
-    echo '[info] torsocks fixed'
+    echo '[info] torsocks fixed.'
     sed -i "s|listen-address 0\.0\.0\.0:8118|listen-address 0\.0\.0\.0:$PRIVOXY_PORT|g" '/etc/privoxy/config'
     sed -i "s|forward-socks5t \/ localhost:9050|forward-socks5t \/ localhost:$TORSOCKS_PORT|g" '/etc/privoxy/config'
-    echo '[info] privoxy fixed'
+    echo '[info] privoxy fixed.'
 else
-    echo ''
-    echo "[info] Torless build detected. Skip fixing torsocks + privoxy configs."
+    echo '[info] Torless build detected. Skip fixing torsocks + privoxy configs.'
 fi
 
 
