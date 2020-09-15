27e1b27591c50fa8a8739064c2d8b301b819549f
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-15 13:35:39 +0100

    Cosmetic

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -48,14 +48,14 @@ tinyproxy -c /etc/tinyproxy/tinyproxy.conf
 
 ### Run TOR+Privoxy depending on build ###
 if [[ -f "/usr/sbin/tor" ]]; then
-	echo ''
+    echo ''
     echo "[info] Tor build detected"
     echo "[info] Run tor as service on port $TORSOCKS_PORT"
     service tor start
     echo "[info] Run privoxy in background on port $PRIVOXY_PORT"
     privoxy /etc/privoxy/config
 else
-	echo ''
+    echo ''
     echo "[info] Torless build detected so do nothing"
 fi
 
