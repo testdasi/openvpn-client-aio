c19f3d88bc76b1c9c87967e1f28b37d20e1a2041
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-15 13:47:08 +0100

    Update nftables.sh

--- scripts/nftables.sh
+++ scripts/nftables.sh
@@ -20,16 +20,15 @@ sed -i "s|_TINYPROXY_PORT_|$TINYPROXY_PORT|g" '/nftables.rules'
 
 ### Run TOR+Privoxy depending on build ###
 if [[ -f "/usr/sbin/tor" ]]; then
-    echo "[info] Tor build detected. Editing tor + privoxy ruleset"
+    echo '[info] Tor build detected. Editing tor + privoxy ruleset'
     sed -i "s|_TORSOCKS_PORT_|$TORSOCKS_PORT|g" '/nftables.rules'
     sed -i "s|_PRIVOXY_PORT_|$PRIVOXY_PORT|g" '/nftables.rules'
 else
-    echo ''
-    echo "[info] Torless build detected. Removing tor + privoxy ruleset"
-    sed -i "s|add rule ip filter INPUT iifname "eth0" tcp dport _TORSOCKS_PORT_ ct state new,established counter accept||g" '/nftables.rules'
-    sed -i "s|add rule ip filter INPUT iifname "eth0" tcp dport _PRIVOXY_PORT_ ct state new,established counter accept||g" '/nftables.rules'
-    sed -i "s|add rule ip filter OUTPUT oifname "eth0" tcp sport _TORSOCKS_PORT_ ct state established counter accept||g" '/nftables.rules'
-    sed -i "s|add rule ip filter OUTPUT oifname "eth0" tcp sport _PRIVOXY_PORT_ ct state established counter accept||g" '/nftables.rules'
+    echo '[info] Torless build detected. Removing tor + privoxy ruleset'
+    sed -i 's|add rule ip filter INPUT iifname "eth0" tcp dport _TORSOCKS_PORT_ ct state new,established counter accept||g' '/nftables.rules'
+    sed -i 's|add rule ip filter INPUT iifname "eth0" tcp dport _PRIVOXY_PORT_ ct state new,established counter accept||g' '/nftables.rules'
+    sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _TORSOCKS_PORT_ ct state established counter accept||g' '/nftables.rules'
+    sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _PRIVOXY_PORT_ ct state established counter accept||g' '/nftables.rules'
 fi
 
 ### Add rules ###
