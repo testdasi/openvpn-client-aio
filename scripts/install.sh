a4e76fd844994c29ba16ee429a9c1d5c2f39780d
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 16:01:00 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,7 +19,6 @@ else
     # install torsocks and privoxy
     apt-get -y update \
     && apt-get -y install torsocks privoxy \
-    && apt-get -y install tini \
     && mkdir -p /etc/tor \
     && rm -rf /etc/tor/* \
     && mkdir -p /etc/privoxy \
